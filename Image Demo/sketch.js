04099dbee9764b42bedb44e14d7d01a5cb65d1c7
CompAj <114016409+CompAj@users.noreply.github.com>
2022-09-22 12:13:35 -0600

    Image Demo

---
+++ Image Demo/sketch.js
@@ -0,0 +1,30 @@
+// Image Demo
+// Anjana
+// Sept.22.2022
+
+
+let fishImage;
+let scalar = 3.5;
+
+function preload() {
+  fishImage = loadImage("Fish.png");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+}
+
+function draw() {
+  background(220);
+  image(fishImage, mouseX, mouseY, fishImage.width*scalar, fishImage.height*scalar);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    scalar = scalar * 1.5;
+  }
+  else if (keyCode === DOWN_ARROW) {
+    scalar = scalar * 0.75;
+  }
+}
